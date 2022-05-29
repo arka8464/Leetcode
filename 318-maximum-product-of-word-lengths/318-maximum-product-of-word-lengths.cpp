d6232dae8080623a196c9901206eb2fd05de2cff
arka8464 <69448901+arka8464@users.noreply.github.com>
2022-05-30 02:01:47 +0530

    Time: 191 ms (28.43%), Space: 15.8 MB (63.65%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,22 +1,32 @@
 class Solution {
-   bool checkCommon(bitset<26>& a, bitset<26>& b) {
-	for(int i = 0; i < 26; i++) if (a[i] && b[i]) return true;
-	return false;
-}
+  bool checkCommon(bitset<26>& a,bitset<26>& b)
+  {
+      for(int i=0;i<26;i++)
+      {
+          if(a[i]&&b[i])
+              return true;
+      }
+      return false;
+  }
     public:
     
     int maxProduct(vector<string>& words) {
-        // int max=0;
-      int n = size(words), ans = 0;
-	vector<bitset<26> > chars(n);
-	for(int i = 0; i < n; i++) {
-		for(auto& ch : words[i])  // map the letters to 1 if it occurs in the word
-			chars[i][ch - 'a'] = 1;
-		// now check with all other words and if there's no common letter, then update ans	
-		for(int j = 0; j < i; j++)
-			if(!checkCommon(chars[i], chars[j])) 
-				ans = max(ans, int(size(words[i]) * size(words[j])));
-	}   
-	return ans;
+     int ans=0;
+        int n=words.size();
+        vector<bitset<26>> chars(n);
+        for(int i=0;i<n;i++)
+        {
+            for( char ch :words[i])
+            {
+                chars[i][ch-'a']=1;
+            }
+            for(int j=0;j<i;j++)
+            {
+               
+                if(!checkCommon(chars[i],chars[j]))
+                    ans = max(ans, (int)words[i].size()*(int)words[j].size());
+            }
+        }
+        return ans;
     }
 };

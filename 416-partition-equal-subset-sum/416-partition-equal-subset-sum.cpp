67aa90f93c5869bcc777ebf049961e3752303eda
arka8464 <69448901+arka8464@users.noreply.github.com>
2022-06-01 00:30:30 +0530

    Time: 1940 ms (5.00%), Space: 97.5 MB (9.86%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    public:
+    int dp[201][100000];
+     Solution()
+          {
+           memset(dp,-1,sizeof(dp));   
+          }
+    bool SubsetSum(vector<int>& arr,int n,int sum)
+    {
+         if (sum == 0)
+        return true;
+    if (n == 0)
+        return false;
+        
+          if(dp[n][sum]!=-1)
+          return dp[n][sum];
+     
+       
+         if(arr[n-1]<=sum)
+         {
+             return dp[n][sum]= SubsetSum(arr,n-1,sum-arr[n-1])||SubsetSum(arr,n-1,sum);
+         }
+         else 
+           return dp[n][sum]= SubsetSum(arr,n-1,sum);
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int count=0;
+        for(int i=0;i<nums.size();i++)
+            count+=nums[i];
+        
+        if(count%2!=0)
+            return false;
+        
+        return SubsetSum(nums,nums.size(),count/2); 
+        
+    }
+};

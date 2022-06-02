38ae1141e013ce1cca5ca9bb2ca508c4d332e7bd
arka8464 <69448901+arka8464@users.noreply.github.com>
2022-06-02 18:48:23 +0530

    Time: 98 ms (47.65%), Space: 28.3 MB (93.21%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,7 +1,29 @@
 class Solution {
 public:
+//     void insertion(vector<int>&v,int temp)
+// {
+//    if(v.size()==0 || v[v.size()-1]<=temp)
+//    {
+//    v.push_back(temp);
+//    return;
+//    }
+//    int x=v[v.size()-1];
+//    v.pop_back();
+//    insertion(v,temp);
+//    v.push_back(x);
+// }
+//     void sorting(vector<int>&v)
+// {
+//    if(v.size()<=1)
+//    return;
+//    int temp=v[v.size()-1];
+//    v.pop_back();
+//    sorting(v);
+//    insertion(v,temp);
+// }
+    
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+       sort(nums.begin(),nums.end());
         return nums;
     }
 };

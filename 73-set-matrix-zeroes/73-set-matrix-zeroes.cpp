d6de0d9a7651285bb1ca37e52bac95ab58c85e46
arka8464 <69448901+arka8464@users.noreply.github.com>
2022-06-08 22:28:49 +0530

    Time: 25 ms (27.93%), Space: 13.1 MB (95.91%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,48 +1,52 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
-
-    for (int i = 0; i < rows; i++) {
-        if (matrix[i][0] == 0) col0 = 0;
-        for (int j = 1; j < cols; j++)
-            if (matrix[i][j] == 0)
-                matrix[i][0] = matrix[0][j] = 0;
-    }
-
-    for (int i = rows - 1; i >= 0; i--) {
-        for (int j = cols - 1; j >= 1; j--)
-            if (matrix[i][0] == 0 || matrix[0][j] == 0)
-                matrix[i][j] = 0;
-        if (col0 == 0) matrix[i][0] = 0;
-    }
-    }
-};
-
-
-/*Have some unresolved bug in my code  
-vector<pair<int,int>>v;
-        pair <int,int>p;
-        for(int i=0;i< matrix.size();i++)//this loop is the traverse the matrix and store the location of zero s in a vector of pairs
+    void setZeroes(vector<vector<int>>& m) {
+        bool iscolzero=false;
+        bool isrowzero=false;
+        
+        for(int i=0;i<m.size();i++)
         {
-            for(int j=0;j< matrix[i].size();j++)
-            {
-                if(matrix[i][j]==0)
-                {
-                    v.push_back(make_pair(i,j));
-                } 
+            if(m[i][0]==0){
+               iscolzero=true;
+            break;
             }
         }
-        for(int i=0;i<v.size();i++)
+         for(int i=0;i<m[0].size();i++)
         {
-            for(int j=0;j<matrix[0].size();j++)
-            {
-                matrix[j][v.at(i).first]=0;
+            if(m[0][i]==0){
+               isrowzero=true;
+            break;
             }
-            for(int j=0;j<matrix.size();j++)
+        }
+       for(int i=1;i<m.size();i++)
+       {
+           for(int j=1;j<m[0].size();j++)
+           {
+                if (m[i][j] == 0) {
+                    m[i][0] = 0;
+                    m[0][j] = 0;
+                }
+
+           }
+       }
+        for(int i=1;i<m.size();i++)
+        {
+            for(int j=1;j<m[0].size();j++)
             {
-                matrix[v.at(i).second][j]=0;
+                if(m[0][j]==0||m[i][0]==0)
+                    m[i][j]=0;
             }
         }
         
-*/
+        if(iscolzero)
+        {
+            for(int i=0;i<m.size();i++)
+                m[i][0]=0;
+        }
+        if(isrowzero)
+        {
+            for(int i=0;i<m[0].size();i++)
+                m[0][i]=0;
+        }
+    }
+};

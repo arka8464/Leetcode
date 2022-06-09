f4ed72c9d5e79eed76666c11427183a047d6f887
arka8464 <69448901+arka8464@users.noreply.github.com>
2022-06-09 09:04:42 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (24.57%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,7 +1,25 @@
 class Solution {
+    void insert(vector<int>& nums,int last)
+    {
+        if(nums.size()==0||nums.back()<=last)
+        {
+            nums.push_back(last);
+            return;
+        }
+        int temp =nums.back();
+       nums.pop_back();
+        insert(nums,last);
+        nums.push_back(temp);
+            
+    }
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        
+      if(nums.size()<=1)
+          return;
+       int last=nums.back();
+        // cout<<last<<endl;
+       nums.pop_back();
+        sortColors(nums);
+        insert(nums,last);
     }
 };

561b1c9d38bd47e5d2ccef0f16701ff439ecfe0d
arka8464 <69448901+arka8464@users.noreply.github.com>
2022-06-26 12:41:46 +0530

    Time: 9 ms (21.91%), Space: 7 MB (52.26%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -1,19 +1,41 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        stringstream ss(s);
-        int ans=0;
-        ss>>ans;
-        return ans;
+      
         
-      /*  int sign=+1,i=0;
-        long ans=0;
- int n=s.size();
-        while(s[i]==' '&&i<n)
-            i++
-    */
+        int sign=+1,i=0;
+        int ans=0;
+       int n=s.size();
+       
+    
+         while(s[i]==' '&&i<n)
+                 i++;
+        
+        if(s[i]=='-' || s[i]=='+')
+        {
+            if(s[i]=='-' )
+                sign = -1;
+            i++;
+        }
+        
+       for(; i< s.length() && s[i]>='0' && s[i] <='9';){
+            int no = s[i] - '0';
+            if(((ans) > (INT_MAX/10)) || (ans == INT_MAX / 10 && s[i] - '0' > 7)){
+                return sign ==1 ? INT_MAX : INT_MIN;
+            }        
+            ans = ans*10 + no;
+            i++;
+        }
+        return ans * sign;
             
         
     
     }
 };
+
+/* STL shiz
+  stringstream ss(s);
+        int ans=0;
+        ss>>ans;
+        return ans;
+*/

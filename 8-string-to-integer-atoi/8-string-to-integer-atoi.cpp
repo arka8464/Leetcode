a67f6f2e36866becf7a7843d0127a09e0ecc13b7
arka8464 <69448901+arka8464@users.noreply.github.com>
2022-06-26 12:06:42 +0530

    Time: 4 ms (66.60%), Space: 6.9 MB (88.95%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        stringstream ss(s);
+        int ans=0;
+        ss>>ans;
+        return ans;
+        
+      /*  int sign=+1,i=0;
+        long ans=0;
+ int n=s.size();
+        while(s[i]==' '&&i<n)
+            i++
+    */
+            
+        
+    
+    }
+};

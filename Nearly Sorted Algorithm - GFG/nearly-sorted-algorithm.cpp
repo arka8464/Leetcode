10dd6f62b49ce89674f97fb5217667a2eb80068c
arka8464 <69448901+arka8464@users.noreply.github.com>
2022-06-27 17:12:47 +0530

    Added solution - LeetHub

--- Nearly Sorted Algorithm - GFG/nearly-sorted-algorithm.cpp
+++ Nearly Sorted Algorithm - GFG/nearly-sorted-algorithm.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+vector<int> help(vector<int>& v,int k)
+{
+    priority_queue<int,vector<int>,greater<int>> pq;
+    vector<int> ans;
+   int i=0,n=v.size();
+   for(;i<k;i++)
+    pq.push(v[i]);
+   
+    for(;i<n;i++)
+    {
+        ans.push_back(pq.top());
+        pq.pop();
+        pq.push(v[i]);
+    }
+    while(!pq.empty())
+    {
+        ans.push_back(pq.top());
+        pq.pop();
+    }
+    return ans;
+}
 
 int main() {
 	int t;
@@ -9,15 +30,17 @@ int main() {
 	{
 	    int n1,n2;
 	    cin>>n1>>n2;
-	    vector<int>v;
-	    for(int i=0;i<n1;i++){
+	    vector<int>v,ans;
+	    for(int i=0;i<n1;i++)
+	    {
 	        int temp;
 	        cin>>temp;
 	        v.push_back(temp);
 	    }
-	    sort(v.begin(),v.end());
+	    //sort(v.begin(),v.end());
+	   ans= help(v,n2);
 	    for(int i=0;i<n1;i++)
-	      cout<<v[i]<<" ";
+	      cout<<ans[i]<<" ";
 	    cout<<endl;  
 	     
 	}

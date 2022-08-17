98e50fc186eb058f0fbe7c2bc0456ecfb3836c2b
arka8464 <69448901+arka8464@users.noreply.github.com>
2022-08-17 21:01:24 +0530

    Time: 6 ms (63.44%), Space: 12 MB (74.79%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,9 +1,33 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-       next_permutation(nums.begin(),nums.end()); 
+        int n=nums.size(),i,j;
+        for( i=n-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+                break;
+        }
+        if(i<0)
+        {
+            reverse(nums.begin(),nums.end());
+        }
+        else
+        {
+            for(j=n-1;j>=0;j-- )
+            {
+                if(nums[j]>nums[i])
+                    break;
+            }
+            swap(nums[i],nums[j]);
+            reverse(nums.begin()+i+1,nums.end());
+        }
     }
 };
 
-//INBUILT STL
+// INBUILT STL
 //  next_permutation(nums.begin(),nums.end());
+
+// The next approach is generating all the permutations and then storing it in the ans vector of vector and then iterating it to find the next iteration 
+
+//T.C will be O(n!* n)
+

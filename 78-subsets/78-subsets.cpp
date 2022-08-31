ec98e96a3cff7f9819d0a62cf6443262856b04d4
arka8464 <69448901+arka8464@users.noreply.github.com>
2022-08-31 11:04:19 +0530

    Time: 0 ms (100.00%), Space: 7 MB (87.46%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,22 +1,44 @@
-class Solution {
+// class Solution {
    
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         int n =nums.size();
+//         int total_subsets=(1<<n);
+//         for(int mask=0;mask<total_subsets;mask++)
+//         {
+//             vector<int >temp;
+//             for(int i=0;i<n;i++)
+//             {
+//                 if((mask&(1<<i))!=0)
+//                 {
+//                    temp.push_back(nums[i]); 
+//                 }
+//             }
+//             ans.push_back(temp);
+//         }
+//         return ans;
+//     }
+// };
+
+class Solution {
+    void subsets(vector<vector<int>>&ans,int ind,vector<int>&nums,vector<int>&ds)
+    {
+        if(ind==nums.size())
+        {
+            ans.push_back(ds);
+            return;
+        }
+        ds.push_back(nums[ind]);
+         subsets(ans,ind+1,nums,ds);
+         ds.pop_back();
+         subsets(ans,ind+1,nums,ds);
+    }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        int n =nums.size();
-        int total_subsets=(1<<n);
-        for(int mask=0;mask<total_subsets;mask++)
-        {
-            vector<int >temp;
-            for(int i=0;i<n;i++)
-            {
-                if((mask&(1<<i))!=0)
-                {
-                   temp.push_back(nums[i]); 
-                }
-            }
-            ans.push_back(temp);
-        }
+         vector<vector<int>> ans;
+        vector<int>ds;
+         subsets( ans,0,nums,ds);
         return ans;
     }
 };

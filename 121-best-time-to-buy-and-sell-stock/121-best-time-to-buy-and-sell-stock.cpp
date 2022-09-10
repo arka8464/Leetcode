041b3a01b915d259d47f2cc1a873a96b4ef5600a
arka8464 <69448901+arka8464@users.noreply.github.com>
2022-09-10 19:32:44 +0530

    Time: 376 ms (5.02%), Space: 93.3 MB (89.29%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         //doing using kadane s algo
-      int maxP=0;
-        int mini=INT_MAX;
-        for(int i=0;i<prices.size();i++)
-        {
-            mini=min(mini,prices[i]);
-            maxP=max(maxP,prices[i]-mini);
-        }
-        return maxP;
+     int pft=0;
+     int mini=prices[0];
+    for(int i=1;i<prices.size();i++)
+    {
+        int pft_temp=prices[i]-mini;
+         pft=max(pft_temp,pft);
+        mini=min(mini,prices[i]);
+    }
+        return pft;
     }
 };

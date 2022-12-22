01b6135eaee3a984aea852f9a639f1715c317bf3
arka8464 <69448901+arka8464@users.noreply.github.com>
2022-12-22 12:14:24 +0530

    Time: 8 ms (91.64%), Space: 7.7 MB (25.35%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -6,37 +6,79 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
       
-        unordered_map<ListNode *,bool>m;
+        // unordered_map<ListNode *,bool>m;
         ListNode *slow=head;
-                ListNode *fast=head;
-        bool flag=true;
-        while(fast&&fast->next)
-        {
-            slow=slow->next;
-            fast=fast->next->next;
-            if(slow==fast)
+        ListNode *fast=head;
+        // bool flag=true;
+       while(fast != NULL && fast->next != NULL)
+       {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast)
             {
-                flag=false;
-                break;
+                slow = head;
+                while(slow != fast)
+                {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
             }
-        }
-        if(flag)return NULL;
+           }
+        
+        // if(flag)return NULL;
             
-        ListNode *curr=head;
+//         ListNode *curr=head;
         
         
-        while(curr)
-        {
-            if(m[curr]==true)return curr;
+//         while(curr)
+//         {
+//             if(m[curr]==true)return curr;
                 
-            m[curr]=true;
+//             m[curr]=true;
             
-            curr=curr->next;
-        }
+//             curr=curr->next;
+//         }
         return NULL;
     }
 };
+//this takes O(n) TC but has a O(n) SC hence required to optimise further
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *head) {
+      
+//         unordered_map<ListNode *,bool>m;
+//         ListNode *slow=head;
+//                 ListNode *fast=head;
+//         bool flag=true;
+//         while(fast&&fast->next)
+//         {
+//             slow=slow->next;
+//             fast=fast->next->next;
+//             if(slow==fast)
+//             {
+//                 flag=false;
+//                 break;
+//             }
+//         }
+//         if(flag)return NULL;
+            
+//         ListNode *curr=head;
+        
+        
+//         while(curr)
+//         {
+//             if(m[curr]==true)return curr;
+                
+//             m[curr]=true;
+            
+//             curr=curr->next;
+//         }
+//         return NULL;
+//     }
+// };

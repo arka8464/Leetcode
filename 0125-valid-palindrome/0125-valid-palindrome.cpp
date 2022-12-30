5c2f55e095a1e097a1aec26c64d72de1e87f8e73
arka8464 <69448901+arka8464@users.noreply.github.com>
2022-12-30 11:20:31 +0530

    Time: 12 ms (48.02%), Space: 7.8 MB (27.52%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -8,13 +8,19 @@ public:
             s[i]=tolower(s[i]);
         } 
 
+
         string s1;
-         for(int i=0;i<s.size();i++)
+       for (char c : s)
+    {
+        if ((c >= '0' && c <= '9')  || (c >= 'a' && c <= 'z'))
         {
-            if(isalnum(s[i]))
-           s1.push_back(s[i]);                    
-         } 
-        
+            s1.push_back( c);
+        }
+    }
+        cout<<s.size()<<endl;
+        cout<<s<<endl;
+        cout<<s1.size()<<endl;
+        cout<<s1<<endl;
         s=s1;
         reverse(s.begin(),s.end());
               

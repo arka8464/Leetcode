6c880b0abb4d43416a759c512ca0aeffee6df75b
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-01-21 11:25:44 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (20.07%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
-  int fib(vector<int>&dp,int n)
-  {
-       if(n<2)
-           return dp[n]= n;
-        if(dp[n]!=-1)return dp[n];
-        return dp[n]=fib(dp,n-1)+fib(dp,n-2);
-  }
     int fib(int n) {
-        vector<int> dp(101,-1);
-       return fib(dp,n);
+       vector<int>dp(n+1,-1);
+        if(n<2)return n;
+    dp[0]=0,dp[1]=1;
+        for(int i=2;i<=n;i++)
+            dp[i]=dp[i-1]+dp[i-2];
+        return dp[n];
     }
 };

6f6ad80897e8c2ce1c00cfb8ca28bb4ea02b1712
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-01-31 20:52:15 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (31.04%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,4 +1,33 @@
 class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0&&j==0)
+                {
+                    dp[0][0]=1;
+                continue; 
+                }
+                 int up=0;
+          int left = 0;
+          
+          if(i>0) 
+            up = dp[i-1][j];
+          if(j>0)
+            left = dp[i][j-1];
+            
+          dp[i][j] = up+left;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
+/*
+class Solution {
     int uniquePaths(int m, int n,vector<vector<int>>&dp)
     {
          if(n==0&&m==0) 
@@ -17,3 +46,4 @@ public:
      return  uniquePaths( m-1,  n-1,dp);
     }
 };
+*/

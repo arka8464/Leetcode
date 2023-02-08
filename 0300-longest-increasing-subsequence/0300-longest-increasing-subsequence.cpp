d68fe723cf9e7c5986c6482cb5efde78650c2910
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-02-08 11:21:11 +0530

    Time: 733 ms (23.61%), Space: 292.1 MB (20.05%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,24 +1,37 @@
 class Solution {
-    int f(vector<int>& nums,int curr_ind,int prev_ind,vector<vector<int>>& dp)
-    {
-        if(curr_ind==nums.size())
-            return 0;
+//     int f(vector<int>& nums,int curr_ind,int prev_ind,vector<vector<int>>& dp)
+//     {
+//         if(curr_ind==nums.size())
+//             return 0;
        
-         if(dp[curr_ind][prev_ind+1]!=-1)
-            return dp[curr_ind][prev_ind+1];
+//          if(dp[curr_ind][prev_ind+1]!=-1)
+//             return dp[curr_ind][prev_ind+1];
         
-        int take=-1e8;
-        int nottake=f(nums,curr_ind+1,prev_ind,dp);
-        if(prev_ind==-1||nums[curr_ind]>nums[prev_ind])
-        {
-            take=1+f(nums,curr_ind+1,curr_ind,dp);
-        }
-        return dp[curr_ind][prev_ind+1]=max(take,nottake);
-    }
+//         int take=-1e8;
+//         int nottake=f(nums,curr_ind+1,prev_ind,dp);
+//         if(prev_ind==-1||nums[curr_ind]>nums[prev_ind])
+//         {
+//             take=1+f(nums,curr_ind+1,curr_ind,dp);
+//         }
+//         return dp[curr_ind][prev_ind+1]=max(take,nottake);
+//     }
 public:
     int lengthOfLIS(vector<int>& nums) {
         
-        vector<vector<int>>dp(nums.size(),vector<int>(nums.size()+1,-1));
-        return f(nums,0,-1,dp);
+        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,0));
+        for(int curr_ind=nums.size()-1;curr_ind>=0;curr_ind--)
+        {
+            for(int prev_ind=curr_ind-1;prev_ind>=-1;prev_ind--)
+            {
+                int nottake= dp[curr_ind+1][prev_ind+1];
+                int take=-1e8;
+            
+                if(prev_ind==-1||nums[curr_ind]>nums[prev_ind])
+                         take=1+dp[curr_ind+1][curr_ind+1];
+              
+                dp[curr_ind][prev_ind+1]=max(take,nottake);
+            }
+        }
+        return dp[0][-1+1] ;
     }
 };

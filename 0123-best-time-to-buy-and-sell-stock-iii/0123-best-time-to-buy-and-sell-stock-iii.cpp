7147d8b54aefcd56179666d418dc93f8f5189feb
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-02-19 23:40:11 +0530

    Time: 1007 ms (7.83%), Space: 285.3 MB (5.01%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,24 +1,27 @@
 class Solution {
-    int f(vector<int>& prices, vector<vector<vector<int>>>&dp,int ind,int canBuy,int n,int limit)
+    int f(vector<int>& prices,int canBuy,int ind,int sz,int trNo,vector<vector<vector<int>>> &dp)
     {
-        //base case
-        if(limit==0)return 0;
-        if(ind==n)return 0;
-        if(dp[ind][canBuy][limit]!=-1)
-             return dp[ind][canBuy][limit];
-        int profit=0;
-        if(canBuy==1)
-            profit=max(f(prices,dp,ind+1,1,n,limit),-prices[ind]+f(prices,dp,ind+1,0,n,limit));
-        else
-            profit=max(f(prices,dp,ind+1,0,n,limit),prices[ind]+f(prices,dp,ind+1,1,n,limit-1));
-
-        return dp[ind][canBuy][limit]=profit;
+        //base
+        if(trNo==0)
+            return 0;
+        if(ind==sz)
+            return 0;
         
+        if(dp[ind][canBuy][trNo]!=-1)
+            return dp[ind][canBuy][trNo];
+        int profit=0;
+        // cout<<ind<<endl;
+        if(canBuy==1)
+         profit= max (f(prices,1,ind+1,sz,trNo,dp),-prices[ind]+f(prices,0,ind+1,sz,trNo,dp));
+        else
+         profit =max(f(prices,0,ind+1,sz,trNo,dp),prices[ind]+f(prices,1,ind+1,sz,trNo-1,dp));
+
+        return dp[ind][canBuy][trNo]=profit;
     }
 public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return f(prices,dp,0,1,n,2);
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(3,vector<int>(3,-1)));
+        return f(prices,1,0,prices.size(),2,dp);   
     }
 };

276d9304e297e785e15507c8da6799e6b08f4371
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-02-21 02:05:42 +0530

    Time: 34 ms (25.81%), Space: 15.1 MB (20.32%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,55 +1,39 @@
 class Solution {
-private :
-    void dfs(vector<int> &vis,vector<int> adjL[],int start)
-    {
-        vis[start]=1;
-        
-        for(auto it:adjL[start])
-        {
-            if(vis[it]==0)
+            void dfs(vector<int>adj[],int start,vector<int> &vis)
             {
-                dfs(vis,adjL,it);
+                vis[start]=1;
+                for(auto it: adj[start])
+                {
+                    if(vis[it]==0)
+                    dfs(adj,it,vis);
+                }
             }
-        }
-    }
-    public:
-    
-    
+
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int V=isConnected[0].size();
-        vector<int>adjL[V];
         
-        for(int i=0;i<V;i++)
+        int V=isConnected.size();
+        vector<int>adj[V];
+        
+        for(int i=0;i<isConnected.size();i++)
         {
-            for(int j=0;j<V;j++)
+            for(int j=0;j<isConnected[i].size();j++)
             {
                 if(isConnected[i][j]==1)
                 {
-                  adjL[i].push_back(j);
-                  adjL[j].push_back(i);
+                    adj[j].push_back(i);
+                    adj[i].push_back(j);//coverting the adj matrix to adj vector
                 }
             }
         }
-         for(int i=0;i<V;i++)
-        {
-           // cout<<adjL[i][0]<<" ";
-          for(int j=0;j<adjL[i].size();j++)
-            {
-          cout<<adjL[i][j]<<" ";
-        }
-             cout<<endl;
-        }
-        
-        vector<int>vis(V,0);
+        vector<int> vis(V,0);
         int ans=0;
         for(int i=0;i<V;i++)
         {
-            if(vis[i]==0){
-                                                ans++;
-
-                dfs(vis,adjL,i);
-
-            }
+            if(vis[i]==0)
+                   {ans++;
+                    dfs(adj,i,vis);
+                   }
         }
         return ans;
     }

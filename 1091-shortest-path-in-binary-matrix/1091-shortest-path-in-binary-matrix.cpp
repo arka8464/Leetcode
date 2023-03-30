17e8e759ad40199d334cc5c0485d7d7956ed1c78
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-03-30 20:43:43 +0530

    Time: 78 ms (51.02%), Space: 21.9 MB (38.04%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,53 +1,38 @@
 class Solution {
 public:
-    
-    bool isValid(vector<vector<int>>& grid, int i, int j, int n, vector<vector<bool>>& visited){
-        
-        return (i>=0 and i<n and j>=0 and j<n and grid[i][j]==0 and !visited[i][j]);
-        
-    }
-    
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        if(n==1&&m==1&&grid[0][0]==0)
+            return 1;
+        if(grid[0][0] == 1 || grid[n-1][m-1] == 1) return -1;
         
-        int n = grid.size();
-        vector<vector<bool>> visited(n, vector<bool> (n, false));
-        queue<pair<int, int>> q;
-        int ans = 0;
-        int nodesPushed;
-        
-        if(grid[0][0] == 0){
-            q.push({0, 0});
-            visited[0][0] = true;
-        }
-                
-        while(!q.empty()){
-            
-            nodesPushed = q.size();
-            ans++;
-            
-            for(int cnt = 0; cnt < nodesPushed; cnt++){
-                
-                pair<int, int> frontNode = q.front();
-                q.pop();
-            
-                int i = frontNode.first, j = frontNode.second;
+        vector<vector<int>> dis(grid.size(), vector<int>(grid[0].size(), 1e8));
+        dis[0][0] = 0;
+        queue<pair<pair<int, int>, int>> pq; //{row,col}, wt
+        pq.push({{0, 0}, 1});
 
-                if(i==n-1 and j==n-1) return ans;
+        while(!pq.empty()) {
+            auto frontRow = pq.front().first.first;
+            auto frontCol = pq.front().first.second;
+            auto frontDis = pq.front().second;
+            pq.pop();
 
-                for(int k = i - 1; k <= i + 1 ; k++){
-                    for(int l = j - 1; l <= j + 1; l++){
-                        if(isValid(grid, k, l, n, visited)){
-                            q.push({k, l});
-                            visited[k][l] = true;
-                        }
+            for(int row = -1; row <= 1; row++) {
+                for(int col = -1; col <= 1; col++) {
+                    int newRow = frontRow + row;
+                    int newCol = frontCol + col;
+
+                    if(newRow >= 0 && newRow < grid.size() && newCol >= 0 && newCol < grid[0].size()
+                      && frontDis + 1 < dis[newRow][newCol] && grid[newRow][newCol] == 0) {
+                        dis[newRow][newCol] = frontDis + 1;
+                        if(newRow == grid.size() - 1 && newCol == grid[0].size() - 1)
+                            return dis[newRow][newCol];
+                        pq.push({{newRow, newCol}, frontDis + 1});
                     }
-                }                
-                
-            }            
-            
+                }
+            }
         }
-        
         return -1;
-        
     }
 };

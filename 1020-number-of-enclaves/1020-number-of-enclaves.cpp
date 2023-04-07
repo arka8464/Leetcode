0c84826593710d294f522028f6af2c17698e893a
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-04-07 19:45:51 +0530

    Time: 459 ms (5.01%), Space: 32.1 MB (38.54%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,61 +1,83 @@
 class Solution {
-    void dfs(int row,int col,vector<vector<int>>& grid,vector<vector<int>>& vis)
+    void dfs(int r,int c,vector<vector<int>>& grid)
     {
-        if(row<0||col<0||row>=grid.size()||col>=grid[0].size()||grid[row][col]==0||vis[row][col]==2)
-            return ;
-        vis[row][col]=2;
-        dfs(row-1,col,grid,vis);
-        dfs(row,col-1,grid,vis);
-        dfs(row,col+1,grid,vis);
-        dfs(row+1,col,grid,vis);
+
+        if(r<0||r>=grid.size()||c<0||c>=grid[0].size())
+            return;
+        if(grid[r][c]==0)return;
+                cout<<r<<" "<<c<<endl;
+
+        // cout<<r<<" "<<c<<endl;
+        grid[r][c]=0;
+                                                cout<<"hello4"<<endl;
+
+        dfs(r-1,c,grid);
+                                                        cout<<"hello5"<<endl;
+
+        dfs(r,c-1,grid);
+                                                        cout<<"hello6"<<endl;
+
+        dfs(r+1,c,grid);
+                                                        cout<<"hello7"<<endl;
+
+        dfs(r,c+1,grid);
+
     }
 public:
     int numEnclaves(vector<vector<int>>& grid) {
-        
-        int r=grid.size();
-        int c=grid[0].size();
-        vector<vector<int>> vis(r,vector<int>(c,0));
-        
-        for(int i=0;i<c;i++)
+       int ans=0;
+          int rw=grid.size();
+        int col=grid[0].size();
+       if(rw==1&&col==1)
+           return 0;
+
+        for(int i=0;i<rw;i++)
         {
-            // cout<<"a"<<endl;
-            if(grid[0][i]==1)
-            dfs(0,i,grid,vis);//first row
-        }
-        for(int i=0;i<c;i++)
-        {
-            // cout<<"b"<<endl;
-            if(grid[r-1][i]==1)
-            dfs(r-1,i,grid,vis);//last row
-        }
-        for(int i=0;i<r;i++)
-        {
-            // cout<<"c"<<endl;
+                    cout<<"hello1"<<endl;
+
             if(grid[i][0]==1)
-            dfs(i,0,grid,vis);//first col
+              dfs(i,0,grid);//first col
         }
-         for(int i=0;i<r;i++)
+        for(int i=0;i<rw;i++)
         {
-             // cout<<"d"<<endl;
-             if(grid[i][c-1]==1)
-             dfs(i,c-1,grid,vis);//last col
+                                cout<<"hello2"<<endl;
+
+            if(grid[i][col-1]==1)
+            dfs(i,col-1,grid);//last col
+        }
+        for(int i=0;i<col;i++)
+        {
+                                cout<<"hello3"<<endl;
+
+            if(grid[0][i]==1)
+            dfs(0,i,grid);//first row
+        }
+        for(int i=0;i<col;i++)
+        {
+
+            if(grid[rw-1][i]==1)
+            dfs(rw-1,i,grid);//last row
         }
         
-        int ans=0;
-         for(int i=0;i<r;i++)
+//          for(int i=0;i<rw;i++)
+//         {
+//             for(int j=0;j<col;j++ )
+//             {
+//                 cout<<grid[i][j]<<" ";
+                
+//             }
+//              cout<<endl;
+//         } 
+        // int ans=0;
+         for(int i=0;i<rw;i++)
         {
-            for(int j=0;j<c;j++)
+            for(int j=0;j<col;j++ )
             {
-               cout<<vis[i][j]<<" ";
-            }
-             cout<<endl;
-        }
-        for(int i=0;i<r;i++)
-        {
-            for(int j=0;j<c;j++)
-            {
-                if(vis[i][j]!=2&&grid[i][j]==1)
-                    { ans++;}
+                if(grid[i][j]==1)
+                {
+                    ans++;
+                    // dfs(i,j,grid);
+                }
             }
         }
         return ans;

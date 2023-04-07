1d384551a68bad5ab5b08203f27952c25d794b4d
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-04-07 20:21:13 +0530

    Time: 81 ms (88.04%), Space: 34.3 MB (49.13%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -0,0 +1,91 @@
+class DisjointSet {
+    public:
+    vector<int> rank, parent, size; 
+    DisjointSet(int n) {
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+        }
+    }
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findUPar(parent[node]); 
+    }
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+        }
+        else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        }
+        else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+};
+class Solution {
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+       int n=accounts.size();
+        DisjointSet ds(n);
+        unordered_map<string,int>m;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=1;j<accounts[i].size();j++)
+            {
+                string mail=accounts[i][j];
+                if(m.find(mail)==m.end())
+                    m[mail]=i;
+                else
+                {
+                    ds.unionBySize(i, m[mail]);
+                }
+            }
+        }
+        
+        vector<string> mergedMail[n];
+        
+        for(auto it:m)
+        {
+            string mail=it.first;
+            int node=ds.findUPar(it.second);
+            mergedMail[node].push_back(mail);
+        }
+        
+        vector<vector<string >> ans;
+        for(int i=0;i<n;i++)
+        {
+            if(mergedMail[i].size()==0)continue;
+            sort(mergedMail[i].begin(),mergedMail[i].end());
+            vector<string> tmp;
+            tmp.push_back(accounts[i][0]);
+            for(auto it:mergedMail[i])
+            {
+                tmp.push_back(it);
+            }
+            ans.push_back(tmp);
+        }
+        return ans;
+    }
+};

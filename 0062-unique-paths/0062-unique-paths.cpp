e1918f2070db7485fa7fc5d5f8dbbbbac3ba3b33
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-04-08 20:32:21 +0530

    Time: 2 ms (45.21%), Space: 6.5 MB (33.69%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,49 +1,23 @@
 class Solution {
-public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(i==0&&j==0)
-                {
-                    dp[0][0]=1;
-                continue; 
-                }
-                 int up=0;
-          int left = 0;
-          
-          if(i>0) 
-            up = dp[i-1][j];
-          if(j>0)
-            left = dp[i][j-1];
-            
-          dp[i][j] = up+left;
-            }
-        }
-        return dp[m-1][n-1];
-    }
-};
-/*
-class Solution {
-    int uniquePaths(int m, int n,vector<vector<int>>&dp)
+    int f(int r, int c,int row,int col,vector<vector<int>> &dp)
     {
-         if(n==0&&m==0) 
-            return dp[0][0]=1;
-        if(n<0||m<0)return 0;
-        if(dp[m][n]!=-1)
-            return dp[m][n];
-        int left=uniquePaths(m-1,n,dp);
-        int right=uniquePaths(m,n-1,dp);
-        return dp[m][n]=left+right;
+        if(r<0||r>=row||c<0||c>=col)
+            return 0;
+        if(r==0&&c==0)
+            return 1;
+        if(dp[r][c]!=-1)
+            return dp[r][c];
+        int up=f(r-1, c,row,col,dp);
+        int left=f(r, c-1,row,col,dp);
+        
+        return dp[r][c]=up+left;
     }
-
 public:
     int uniquePaths(int m, int n) {
-       vector<vector<int>> dp(m,vector<int>(n,-1));
-     return  uniquePaths( m-1,  n-1,dp);
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        
+        
+        return f(m-1,n-1,m,n,dp);
+        
     }
 };
-*/

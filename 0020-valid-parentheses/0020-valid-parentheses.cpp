9e1153f22044036f46017633eda6c639360d26c0
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-04-10 07:17:40 +0530

    Time: 4 ms (39.51%), Space: 6.3 MB (56.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>s1;
+        // cout<<" start"<<s1.size()<<endl;
+        for(auto i:s)
+        {
+           if(i=='(' or i=='{' or i=='[')
+               s1.push(i);
+           else
+           {
+              if(s1.empty())
+               return false;
+           else if (i==')'&&s1.top()!='(')
+                return false;
+           else if (i==']'&&s1.top()!='[')
+               return false;
+           else if (i=='}'&&s1.top()!='{')
+               return false;
+           // else if(s1.empty())
+           //     return false;
+           s1.pop();
+           }
+        }
+        return s1.size()==0;
+    }
+};

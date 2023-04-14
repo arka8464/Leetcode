4b028e66f2a07afec3f67cb2e8f1fc0f399c33cd
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-04-14 08:06:15 +0530

    Time: 174 ms (5.12%), Space: 24.6 MB (10.04%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,24 +1,22 @@
 class Solution {
-    int LCS(string &text1, string &text2,int ind1,int ind2,vector<vector<int>> &dp)
+    int lcs(string &text1, string &text2,int ind1,int ind2,vector<vector<int>> &dp)
     {
-        if(ind1<0||ind2<0)
-            return 0;
+        if(ind1<0||ind2<0)return 0;
         
         if(dp[ind1][ind2]!=-1)
             return dp[ind1][ind2];
-        
+        int match=0;
         if(text1[ind1]==text2[ind2])
-            return 1 + LCS(text1,text2,ind1-1,ind2-1,dp);
-        // if(text1[ind1]!=text2[ind2])
-        else
-            return dp[ind1][ind2]=0 + max(LCS(text1,text2,ind1,ind2-1,dp),LCS(text1,text2,ind1-1,ind2,dp));
-
+         match=1+lcs(text1,text2, ind1-1, ind2-1,dp);
+        
+        int notMatch=0+max(lcs(text1,text2, ind1-1, ind2,dp),lcs(text1,text2, ind1, ind2-1,dp));
+        
+        return dp[ind1][ind2]=max(match,notMatch);
     }
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        int l1=text1.size();
-        int l2=text2.size();
-        vector<vector<int>> dp(l1,vector<int>(l2,-1));
-        return LCS(text1,text2,l1-1,l2-1,dp);
+        
+        vector<vector<int>> dp(text1.size(),vector<int>(text2.size(),-1));
+        return lcs(text1,text2,text1.size()-1,text2.size()-1,dp);
     }
 };

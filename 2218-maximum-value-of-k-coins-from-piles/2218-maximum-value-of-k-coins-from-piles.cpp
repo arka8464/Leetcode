ff7b13057b09c1f3df06fc31b46db92601d1be2e
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-04-15 19:18:55 +0530

    Time: 294 ms (58.38%), Space: 18.5 MB (43.19%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -9,11 +9,11 @@ class Solution {
         
         int notPick=f(ind+1,piles,k,dp);
         int pick=0;
-        int currSum=0;
+        // int currSum=0;
         for(int i=0;i<piles[ind].size()&&i<k;i++)
          {
-            currSum+=piles[ind][i];
-            pick=max(pick,currSum+f(ind+1,piles,k-i-1,dp));
+            // currSum+=piles[ind][i];
+            pick=max(pick,piles[ind][i]+f(ind+1,piles,k-i-1,dp));
         }
             
         return dp[ind][k]=max(pick,notPick);
@@ -21,9 +21,9 @@ class Solution {
 public:
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
         //do prefix sum of all elements so that when we pick the latter element then sum of the lower elements can automatically be calculated 
-        // for(int i=0;i<piles.size();i++)
-        //     for(int j=1;j<piles[i].size();j++)
-        //         piles[i][j]=piles[i][j-1]+piles[i][j];
+        for(int i=0;i<piles.size();i++)
+            for(int j=1;j<piles[i].size();j++)
+                piles[i][j]=piles[i][j-1]+piles[i][j];
  
         for(auto it:piles)
           {

33e208d9175e97dd253e70d071995c3cd5144153
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-04-27 21:14:02 +0530

    Time: 0 ms (100.00%), Space: 6 MB (63.99%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+//         vector<bool> v(n,1);
+        
+//         for(int i=2;i<=n;i++)
+//             v[i]=!v[i]; 
+
+//         int cnt=0;
+        
+//         for(int i=1;i<=n;i++)
+//             if(v[i])
+//                 cnt++;
+        
+//         return cnt;
+
+        return sqrt(n);
+        
+    
+    }
+};

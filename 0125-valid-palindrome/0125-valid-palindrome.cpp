04c3da3e486c2720488602b1e5458a9c83726ca3
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-05-27 10:32:35 +0530

    Time: 8 ms (32.69%), Space: 7.8 MB (20.23%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,35 +2,17 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         
-        s.erase(remove(s.begin(),s.end(),' '),s.end());
+        string newS;
         for(int i=0;i<s.size();i++)
         {
-            s[i]=tolower(s[i]);
-        } 
-
-
-        string s1;
-       for (char c : s)
-    {
-        if ((c >= '0' && c <= '9')  || (c >= 'a' && c <= 'z'))
-        {
-            s1.push_back( c);
+            if(isalnum(s[i]))
+               {
+                newS+=tolower(s[i]);
+            }
         }
-    }
-        cout<<s.size()<<endl;
-        cout<<s<<endl;
-        cout<<s1.size()<<endl;
-        cout<<s1<<endl;
-        s=s1;
-        reverse(s.begin(),s.end());
-              
-        cout<<s<<endl;
-        cout<<s1<<endl;
-
-        if(s==s1)return true;
-        return false;
+        string newS2=newS;
+        reverse(newS.begin(),newS.end());
         
-
-        // return true;
+        return newS2==newS;
     }
 };

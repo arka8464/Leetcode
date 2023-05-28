e10d47d2891ade8b4a3c951f09515573a5fa99f7
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-05-28 22:03:54 +0530

    Time: 62 ms (13.39%), Space: 23.8 MB (90.65%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-         vector<int> ans(nums.size(), -1);
-        stack<int>s;
-        //striver video refered
         
-        for(int i = 0; i < 2*nums.size()-1; i++) {
-            
-            while(!s.empty() && nums[s.top()] < nums[i%nums.size()]) {
-               
-                ans[s.top()] = nums[i%nums.size()];
-                s.pop(); 
+        
+        vector<int>ans(nums.size(),-1);
+        
+        stack<int>st;
+        
+        for(int i=0;i<2*nums.size()-1;i++)//imagine korbo je 2x len er array ta traverse korchi
+        {
+            while(!st.empty()&&nums[st.top()]<nums[i%nums.size()])
+            {
+                ans[st.top()]=nums[i%nums.size()];//ekhane prothom dik theke traverse korchi tai //st.top() jodi choto hoi tr mane etai amar next greater tai push marchi
+                st.pop();
             }
-            s.push(i%nums.size()); 
+             st.push(i%nums.size()); //ekhane direct index ta kei push korchi jate //alada kore map na banate hoi r jehetu arr ta 2*num.size obdhi jache tai ota ke % //lagiyechi
         }
+
+        
         return ans;
     }
 };

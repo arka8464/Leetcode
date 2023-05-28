7398ebd736dcc7ba58087c5311ac8de799557c19
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-05-28 21:09:46 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (36.56%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        
         stack<int>st;
-        unordered_map<int,int>mp; //we will be using map as nextgrt array
+        
+        unordered_map<int,int>mp;
+        
         for(int i=nums2.size()-1;i>=0;i--)
         {
             while(!st.empty() && st.top()<=nums2[i])
@@ -17,49 +18,14 @@ public:
         
             st.push(nums2[i]);
         }
-        //matching and putting value in output vector
-        vector<int>out;
+        
+       
+        
+        vector<int>ans;
         for(int i=0;i<nums1.size();i++)
         {
-            out.push_back(mp[nums1[i]]);
+            ans.push_back(mp[nums1[i]]);
         }
-        return out;
-        
+        return ans;
     }
 };
-
-
-
-
-
-
-     //   naive approach 
-// class Solution {
-// public:
-//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-//        vector<int> ans(nums1.size(),-1);
-//         unordered_map<int,int>m;
-//         for(int i=0;i<nums1.size();i++)
-//             m[nums1[i]]=i;
-        
-//         for(int i=0;i<nums2.size();i++)
-//         {
-//             auto it =m.find(nums2[i]);
-//             if(it!=m.end())
-//             {
-//                 for(int j=i+1;j<nums2.size();j++)
-//                 {
-//                     if(nums2[j]>nums2[i])
-//                     {
-//                         int ind=it->second;
-//                         ans[ind]=nums2[j];
-//                         break;
-//                     }
-                 
-//                 }
-//             }
-           
-//         }
-//         return ans;
-//     }
-// };

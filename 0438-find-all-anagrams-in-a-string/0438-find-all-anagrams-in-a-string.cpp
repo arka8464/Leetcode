fc983473b84b34f291d2ba61df0b93f3abbb7c11
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-06-01 21:32:57 +0530

    Time: 12 ms (80.53%), Space: 8.6 MB (66.19%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,49 +1,45 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-      unordered_map<char,int> sv;
-      unordered_map<char,int> pv;
- vector<int>ans;
-       for(int i=0;i<p.size();i++)pv[p[i]]++;
-      
-        int i=0,k=p.size(),j=0;
+        vector<int> hashp(26,0);
+        vector<int> hashs(26,0); 
+        int k=p.size();
+       
         
-        while(j<s.size())
+        for(int i=0;i<k;i++)
         {
+            hashp[p[i]-'a']++;
+        }
+        vector<int>ans;
+        
+      
+        // for(auto it:hashp)
+        //     cout<<it<<" ";
+        // cout<<"hashp"<<endl;
+         // for(auto it:hashs)
+         //    cout<<it<<" ";
+         //  cout<<"hashs"<<endl;
+        
+        int l=0,r=0;
+        while(r<s.size())
+        {
+            hashs[s[r]-'a']++;
+            if(r-l+1<k)
+            {
+                r++;
+                continue;
+            }
+            else if(r-l+1==k)
+            {
+                if(hashp==hashs)
+                    ans.push_back(l);
+                hashs[s[l]-'a']--;
+              l++;
+            }
+       
+            r++;
             
-            if(j-i+1<k)
-            {
-                sv[s[j]]++;
-                j++;
-            }
-            else if(j-i+1==p.size())
-            {
-                
-               sv[s[j]]++;
-
-                cout<<i<<"i "<<j<<"j "<<endl;
-               for (auto it = sv.begin(); it != sv.end(); ) 
-               {
-                           if (it->second == 0) 
-                               it = sv.erase(it);
-                             else    
-                                 ++it;
-               }
-            //       for(auto it:sv)
-            // //    { cout<<it.first<<" "<<it.second<<endl;}
-            // // cout<<"loop"<<endl;
-               if(sv==pv)
-               {
-                   ans.push_back(i);
-               }
-                 sv[s[i]]--;
-              
-               j++,i++; 
-            }
-          
         }
         return ans;
-         
-        
     }
 };

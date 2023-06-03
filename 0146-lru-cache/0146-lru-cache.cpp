991bf02df79f1f84cb3f7c21f9d006d5b3a8560d
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-06-03 11:54:50 +0530

    Time: 602 ms (5.06%), Space: 177.4 MB (25.70%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,50 +1,58 @@
 class LRUCache {
-   
+    map<int,list<int>::iterator > address;// to access the key  address fast without traversing the whole map
+    // key ,iterator to the address
+    map<int,int>mp;//to keep track of key,value pair
+    list<int> l;// [double ended linked list]to keep track of the keys begin used
+    //notun element gulo front e thakbe ar purono gulo pichone 
+    int cap;//max capacity
+    int sz;//current size of the list
 public:
-    unordered_map<int,int>m;
-    unordered_map<int,list<int>::iterator> addr;
-
-    list<int>l;
-    int cap;
-    int siz;
     LRUCache(int capacity) {
+        sz=0;
         cap=capacity;
-        siz=0;
     }
     
     int get(int key) {
+        /*
+        returns the value if key present and push to list last   
+        */
+        if(mp.find(key)==mp.end())
+            return -1;// not in the list
         
-      if(m.find(key)==m.end())return -1;  //see if the element is present or not 
-    
-       l.erase(addr[key]);
-         addr.erase(key);
+        auto it=address[key];// list e element er location
+        l.erase(it);// delete korlm list theke 
         l.push_front(key);
-         addr[key]=l.begin();
- 
-         return m[key]; 
+        address[key]=l.begin();//notun location ta address e update korlm
+        return mp[key];
+        
+        
     }
-    
+    /*
+    eta new key hole last e dokhabe na hole key already present hole key update korbe ar last e pathabe 
+   sz==cap hoye gele list er last element ta uriye debo
+   */
     void put(int key, int value) {
-        if(m.find(key)!=m.end())//element is already 
+        
+         if(mp.find(key)!=mp.end())     
+         {
+              auto it=address[key];// list e element er location
+              l.erase(it);// delete korlm list theke 
+             
+             sz--;// 
+         }
+        if(sz==cap)
         {
-         l.erase(addr[key]);
-         addr.erase(key);
-         m[key]=value;
-         siz--;
+            auto it=l.back();//last element er address nilm
+            address.erase(it);
+            mp.erase(it);
+            l.pop_back();
+            sz--;
         }
-        if(siz==cap)
-        {
-            addr.erase(l.back());
-         m.erase(l.back());
-                        l.pop_back();
-
-         siz--;
-        }
-                l.push_front(key);
-         addr[key]=l.begin();
-         m[key]=value;
-         siz++;
-
+        l.push_front(key);// prothom e pathalm 
+        mp[key]=value;//notun value hole update korlm
+        address[key]=l.begin();//notun address ta o update korlm
+        
+        sz++;
     }
 };
 

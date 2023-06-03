f07281a67a5ae42986d93565c61f1fa45e58398e
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-06-03 10:02:39 +0530

    Time: 94 ms (5.06%), Space: 71.3 MB (76.26%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,61 +11,67 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        //adding if both lists are present
+        ListNode* temp1=l1;
+        ListNode* temp2=l2;
+        ListNode* dummy=new ListNode(-1e9,NULL);
+        ListNode* final= dummy;
         int carry=0;
-        ListNode* dummy = new ListNode(INT_MIN);
-        ListNode* curr=dummy;
         
-        while(l1&&l2)
+        while(temp1&&temp2)
         {
-          int val=l1->val+l2->val+carry;
-          if(val>=10)
-              carry=1;
-            else 
-                carry=0;
-           val=val%10;
-          ListNode * sum=new ListNode(val);
-          cout<<sum->val<<endl;
-            curr->next=sum;
-          curr=sum;
-            l1=l1->next;
-            l2=l2->next;
+            int sum=temp1->val+temp2->val+carry;
+            cout<<"temp1->Val"<<temp1->val<<"temp2->Val"<<temp2->val<<endl;
+            int val=sum%10;
+            carry=0;
+            ListNode* temp=new ListNode(val,NULL);
+            final->next=temp;
+            final=final->next;
+            if(sum+carry>9)
+                carry=1;
+            cout<<"carry"<<carry<<"sum"<<sum<<endl;
+
+            temp1=temp1->next;
+            temp2=temp2->next;
         }
-        cout<<"hello"<<endl;
-        cout<<carry<<endl;
-        while(l1)
-        {
-           int val=l1->val+carry;
-          if(val>=10)
+            while(temp1)
+            {
+            int sum=temp1->val+carry;
+            carry=0;
+            temp1->val=sum%10;
+                
+            if(sum>9)
               carry=1;
-            else 
-                carry=0;
-           val=val%10;
-            ListNode * sum=new ListNode(val);
-          cout<<sum->val<<endl;
-            curr->next=sum;
-          curr=sum;
-            l1=l1->next;
+            
+              final->next=temp1;
+                final=final->next;
+                temp1=temp1->next;
+            }
+        while(temp2)
+            {
+            int sum=temp2->val+carry;
+            carry=0;
+            temp2->val=sum%10;
+                
+            if(sum>9)
+              carry=1;
+            
+              final->next=temp2;
+                final=final->next;
+                temp2=temp2->next;
+            }
+        
+        if(final)
+            cout<<"final-val"<<final->val<<"carry"<<carry<<endl;
+        if(carry==1)
+        {
+            
+          ListNode* temp=new ListNode(1,NULL);
+          final->next=temp;
+          // final=final->next;
         }
         
-         while(l2)
-        {
-           int val=l2->val+carry;
-          if(val>=10)
-              carry=1;
-            else 
-                carry=0;
-           val=val%10;
-            ListNode * sum=new ListNode(val);
-          cout<<sum->val<<endl;
-            curr->next=sum;
-          curr=sum;
-            l2=l2->next;
-        }
-       if(carry>0)
-          {  ListNode * sum=new ListNode(1);
-          cout<<sum->val<<endl;
-            curr->next=sum;
-          curr=sum;}
+        
         return dummy->next;
     }
 };

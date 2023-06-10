8015408653f74772d718eeda2f05410876314602
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-06-10 21:08:11 +0530

    Time: 45 ms (47.83%), Space: 19.1 MB (54.84%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,19 +2,29 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
-         vector<vector<int>> ans;
-        ans.push_back(intervals[0]);
+        vector<vector<int>>ans;
         
+        ans.push_back(intervals[0]);
         for(int i=1;i<intervals.size();i++)
         {
+            // cout<<"ans.back()[1]"<<ans.back()[1]<<endl;
+            // cout<<"intervals[i][0]"<<intervals[i][0]<<endl;
+
             if(ans.back()[1]>=intervals[i][0])
-                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
-            else
             {
-        ans.push_back(intervals[i]);
-                
+               ans.back()[1]=max(ans.back()[1],intervals[i][1]); 
+               
             }
+             else
+                    ans.push_back(intervals[i]);
+            // for(auto it:ans)
+            // {
+            //     for(auto it1:it)
+            //         cout<<it1<<" ";
+            //     cout<<endl;
+            // }
         }
-        return ans;
+    return ans;
+            
     }
 };

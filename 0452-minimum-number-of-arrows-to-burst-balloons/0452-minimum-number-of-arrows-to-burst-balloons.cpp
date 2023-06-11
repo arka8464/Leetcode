5324b49369466e19709f8e5e4f7758c6b1cb00bf
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-06-11 10:33:37 +0530

    Time: 584 ms (8.74%), Space: 89.8 MB (59.60%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,31 +1,23 @@
 class Solution {
+   static bool compare( vector<int>&a, vector<int>&b)
+    {
+        if(a[1]==b[1])
+            return a[0]<b[0];
+        return a[1]<b[1];
+    }
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
-    int noOfArrows=1;
-    sort(points.begin(), points.end()); // strictly increasing order
-    
-        for(auto it:points)
-            for(auto it1:it)
-                cout<<it1<<" ";
-        cout<<endl;
-    for(int i=1; i<points.size(); i++){
-        int currStart=points[i][0]; // Current Interval's start point
-        int currEnd=points[i][1]; // Current Interval's end point
-        int preEnd=points[i-1][1]; // Previous Interval's end point
-
-        if(currStart>preEnd){
-            noOfArrows++;
-            // cout<<"points[i][0]"<<points[i][0]<<endl;
-            // cout<<"points[i][1]"<<points[i][1]<<endl;
-            // cout<<"points[i-1][1]"<<points[i-1][1]<<endl;
-
-            // no need to update minEnd here bcoz we sorted the array in strictly increasing order
+        sort(points.begin(),points.end(),compare);
+        int prevEnd=points[0][1];
+        int ans=1;
+        for(int i=1;i<points.size();i++)
+        {
+            if(points[i][0]>prevEnd)
+            {
+                ans++;
+                prevEnd=points[i][1];
+            }
         }
-        else{
-            points[i][1] = min(currEnd, preEnd); // Same as minEnd (as explained in the template)
-        }
+        return ans;
     }
-
-    return noOfArrows;
-}
 };

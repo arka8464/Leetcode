55e68456311014387ed84064782110205e03c03a
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-06-11 11:18:51 +0530

    Time: 75 ms (81.41%), Space: 28.4 MB (32.60%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.cpp
+++ 0436-find-right-interval/0436-find-right-interval.cpp
@@ -1,20 +1,34 @@
 class Solution {
+    // static bool compare(const vector<int>& a, const vector<int>& b) {
+    //     if (a[1] == b[1])
+    //         return a[0] <= b[0];
+    //     return a[1] < b[1];
+    // }
+
 public:
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        map<int, int> m;
+
+        for (int i = 0; i < intervals.size(); i++) {
+            m[intervals[i][0]] = i;
+        }
+
+        // sort(intervals.begin(), intervals.end(), compare);
+
         vector<int> ans;
-        map<int,int>m;
-        for(int i=0;i<intervals.size();i++)
-            m[intervals[i][0]]=i;
-        
-        for(int i=0;i<intervals.size();i++)
-        {
-            auto it=m.lower_bound(intervals[i][1]);
-            if(it==m.end())
-                ans.push_back(-1);
+
+        for (int i = 0; i < intervals.size(); i++) {
+           //implement changes here 
+            auto it =m.lower_bound(intervals[i][1]);
+            if(it!=m.end())
+            {
+                ans.push_back(it->second);
+            }
             else
-                               ans.push_back(it->second);
+              ans.push_back(-1);
  
         }
+
         return ans;
     }
 };

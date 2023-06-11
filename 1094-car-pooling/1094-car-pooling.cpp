eb5f0a0863bb1a598eb36cb542b654ed987ba48c
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-06-11 12:38:10 +0530

    Time: 18 ms (49.39%), Space: 10.8 MB (27.83%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,32 +1,23 @@
 class Solution {
-    // static bool compare(vector<int>&a,vector<int>&b)
-    // {
-    //     return a[1]<b[1];
-    // }
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-         // for(auto it:trips)
-         //    for(auto it1:it)
-         //        cout<<it1<<" ";
-        vector<pair<int,int>> v;
+        map<int,int>m;
         
-        for(int i=0;i<trips.size();i++)
+        for(auto it:trips)
         {
-            // cout<<i<<" "<<ans.back()[0]<<" "<<ans.back()[1]<<" "<<ans.back()[2];
-          v.push_back(make_pair(trips[i][1],trips[i][0]));
-          v.push_back(make_pair(trips[i][2],-trips[i][0]));
+            int pickup=it[1];
+            int dropoff=it[2];
+         m[pickup] += it[0];
+         m[dropoff] -= it[0];
 
         }
-               sort(v.begin(),v.end());
-
-//         for(auto it:ans)
-//             for(auto it1:it)
-//                 cout<<it1<<" ";
-        int curr_cap=0;
-        for(int i=0;i<v.size();i++)
+        
+        int currCap=0;
+        for(auto it:m)
         {
-            curr_cap+=v[i].second;
-            if(curr_cap>capacity)return false;
+            currCap+=it.second;
+            if(currCap>capacity)
+                return false;
         }
         return true;
     }

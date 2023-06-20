ad747face2c504ba2cc07a93420974eccae8cc2e
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-06-20 12:53:05 +0530

    Time: 10 ms (6.72%), Space: 15.1 MB (37.22%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,18 +1,23 @@
 class Solution {
+    void f(vector<int>nums,int ind,vector<vector<int>>&ans,vector<int>&temp)
+    {
+        if(ind==nums.size())
+           {
+            ans.push_back(temp);
+            return ;
+           }
+        
+        temp.push_back(nums[ind]);//take
+        f(nums,ind+1,ans,temp);
+        temp.pop_back();//backtrack
+         f(nums,ind+1,ans,temp);
+    }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        int maxElement=(1<<nums.size()); //2^n
-         vector<vector<int>> ans;
-        for(int i=0;i<maxElement;i++)//sob gulo number er upor traverse korchi 
-        {
-            vector<int>temp;
-            for(int j=0;j<nums.size();j++) //ebar sob number gulo r upor giye check korbo je ogulo oi digit gulo te bit set kora ache naki
-            {
-               if(i&(1<<j))//ekhane check korchi je bit gulo set kora ache naki
-                   temp.push_back(nums[j]);
-                
-            }
-            ans.push_back(temp);
-        }
-        return ans;    }
+        
+       vector<vector<int>>ans;
+        vector<int>temp;
+         f(nums,0,ans,temp);
+        return ans;
+    }
 };

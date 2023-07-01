9ea416d525d9912ddbc72230ba89c25973389ecd
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-07-01 08:36:21 +0530

    Time: 3 ms (68.38%), Space: 6.1 MB (26.07%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    int dp[31];
     int fib(int n) {
-                memset(dp, -1, sizeof(dp));
-
-        if(n < 2)
+        if(n<=1)
             return n;
+        vector<int> dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=1;
+
         
-        if(dp[n] != -1)
-            return dp[n];
+        for(int i=2;i<=n;i++)
+           {
+            dp[i]=dp[i-1]+dp[i-2];
+        // cout<<dp[i]<<" "<<dp[i-1]<<" "<<dp[i-2]<<endl;
+        }
         
-        return dp[n] = fib(n-1) + fib(n-2);
+        return dp[n];
     }
 };

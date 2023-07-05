03fc6c3e2165abca9d1f2696f8d6e3badc8ce614
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-07-05 10:59:32 +0530

    Time: 4 ms (55.83%), Space: 7.9 MB (27.03%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -15,12 +15,42 @@ class Solution {
          
          return dp[currR][currC]=up+left;
      }
+    
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int r=obstacleGrid.size();
         int c=obstacleGrid[0].size();
         vector<vector<int>> dp(r+1,vector<int>(c+1,-1));
         
-        return f(obstacleGrid,r-1,c-1,dp);
+        // return f(obstacleGrid,r-1,c-1,dp);
+        
+       
+        
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                
+                 if(obstacleGrid[i][j]==1)
+                {
+                    dp[i][j]=0;
+                }
+                else if(i==0&&j==0)
+                {
+                 dp[i][j]=1;
+                }
+                else
+                {
+                    int up=0,left=0;
+                    if(i>0)
+                    up=f(obstacleGrid,i-1,j,dp);
+                    if(j>0)
+                    left=f(obstacleGrid,i,j-1,dp);
+                 dp[i][j]=up+left;
+                }
+            }
+        }
+        
+        return dp[r-1][c-1];
     }
 };

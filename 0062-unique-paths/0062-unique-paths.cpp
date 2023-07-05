005fcd46fbe48b64fec6196754ef48c30dcb2dca
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-07-05 09:24:07 +0530

    Time: 13 ms (22.60%), Space: 6.5 MB (21.50%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,23 +1,35 @@
 class Solution {
-    int f(int r, int c,int row,int col,vector<vector<int>> &dp)
-    {
-        if(r<0||r>=row||c<0||c>=col)
-            return 0;
-        if(r==0&&c==0)
-            return 1;
-        if(dp[r][c]!=-1)
-            return dp[r][c];
-        int up=f(r-1, c,row,col,dp);
-        int left=f(r, c-1,row,col,dp);
-        
-        return dp[r][c]=up+left;
-    }
+    
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
         
-        
-        return f(m-1,n-1,m,n,dp);
-        
+        dp[0][0]=1;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0&&j==0)
+                 continue;
+                else 
+                {
+                    int up=0,right=0;
+                    if(i>0)
+                        up=dp[i-1][j];
+                    if(j>0)
+                        right=dp[i][j-1];
+                    dp[i][j]=up+right;
+                }
+            }
+        }
+         for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cout<<dp[i][j]<<" ";
+            }
+             cout<<endl;
+         }
+       return dp[m-1][n-1]; 
     }
 };

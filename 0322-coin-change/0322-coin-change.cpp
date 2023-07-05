45ead1aabb59ccd37cc4768114e7fddcdbb3fbe4
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-07-05 18:20:49 +0530

    Time: 125 ms (50.16%), Space: 49.9 MB (10.25%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,27 +1,31 @@
 class Solution {
-    int f(vector<int>& coins, int amount,int ind,vector<vector<int>> &dp)
+    int f(vector<int>& coins, int ind,int amount,vector<vector<int>> &dp)
     {
-        if(amount<0)
-            return 1e8;
-        if(ind==0)
+        if(ind>=coins.size()||amount<0)
+            return 1e9;
+        if(ind==coins.size()-1)
         {
-            if(amount%coins[0]==0)
-                return amount/coins[0];
-            return 1e8;
+            if(amount%coins[ind]==0)
+                return dp[ind][amount]=amount/coins[ind];
+            return dp[ind][amount]=1e9;
         }
         if(dp[ind][amount]!=-1)
             return dp[ind][amount];
-        int notpick=f(coins,amount,ind-1,dp);
-        int pick=1e8;
-        if(amount>=coins[ind])
-            pick=1+f(coins,amount-coins[ind],ind,dp);
-        return dp[ind][amount]=min(pick,notpick);
+        int notPick=f(coins,ind+1,amount,dp);
+        int pick=1e9;
+        
+        if(coins[ind]<=amount)
+            pick=1+f(coins,ind,amount-coins[ind],dp);
+        
+        return dp[ind][amount]=min(pick,notPick);
     }
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
-        int ans= f(coins,amount,coins.size()-1,dp);
-        if(ans>=1e8)
+        vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1,-1));
+        
+                int ans= f(coins,0,amount,dp);
+
+        if(ans==1e9)
             return -1;
         return ans;
     }

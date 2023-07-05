c4fdae05f7ff29986b8ea0d77bea3c0b8496ba98
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-07-05 11:53:44 +0530

    Time: 4 ms (86.92%), Space: 9 MB (21.65%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,34 +1,26 @@
 class Solution {
-//     int  minTotal(vector<vector<int>>&triangle,int r,int c, vector<vector<int>> &dp)
-//     {
-//          int n=triangle.size();
-//         if(r==n-1)return triangle[r][c];
-        
-//         if(dp[r][c]!=-1)
-//             return dp[r][c];
-//         int down=  triangle[r][c]+minTotal(triangle,r+1,c,dp);
-//         int dia=  triangle[r][c]+minTotal(triangle,r+1,c+1,dp);
-//         return dp[r][c]=min(down,dia);
-//     }
+    int f(vector<vector<int>>& triangle,int currR,int currC,int R,int C,vector<vector<int>>& dp)
+     {
+        if(currR>=R||currC>=C)
+         {
+             // cout<<" currR "<<currR<<" currC "<<currC<<endl;
+            return 1e9;
+         }
+        if(currR==R-1)
+            return dp[currR][currC]=triangle[currR][currC];
+        if(dp[currR][currC]!=-1)
+            return dp[currR][currC];
+        int down=triangle[currR][currC] + f(triangle,currR+1,currC,R,C,dp);
+        int downRight=triangle[currR][currC] + f(triangle,currR+1,currC+1,R,C,dp);
+        // cout<<" down "<<down<<" downRight "<<downRight<<" currR "<<currR<<" currC "<<currC<<endl;
+
+        return dp[currR][currC]= min(down,downRight);
+    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-       int n=triangle.size();
-       vector<vector<int>> dp(n,vector<int>(n,-1));
-        
-        for(int i=0;i<n;i++)
-        {
-            dp[n-1][i]=triangle[n-1][i];//base case 
-        }
-        for(int i=n-2;i>=0;i--)
-        {
-            for(int j=i;j>=0;j--)
-            {
-                cout<<i<<" "<<j<<endl;
-                int down=  triangle[i][j]+dp[i+1][j];
-                int dia=  triangle[i][j]+dp[i+1][j+1]; 
-                dp[i][j]=min(down,dia);
-            }
-        }
-      return  dp[0][0];
+        int r=triangle.size();
+        int c=triangle[r-1].size();
+        vector<vector<int>> dp(r+1,vector<int>(c+1,-1));
+        return f(triangle,0,0,r,c,dp);
     }
 };

0520f4cac55e292c819d1dbeba93a927a1eb1023
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-07-05 11:19:19 +0530

    Time: 7 ms (93.03%), Space: 10.2 MB (14.84%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,51 +1,27 @@
 class Solution {
-public:
-    int minPathSum(vector<vector<int>>& grid) {
-        int r=grid.size();
-        int c=grid[0].size();
-        vector<vector<int>>dp(r,vector<int>(c,-1));
-       
-        
-        for(int i=0;i<r;i++)
-        {
-            for(int j=0;j<c;j++)
-            {
-                if(i==0&&j==0)
-                {
-                    dp[0][0]=grid[0][0];
-                    continue;
-                }
-                int up=1e9,left=1e9;
-                if(i>0)
-                    up= grid[i][j]+dp[i-1][j];
-                if(j>0)
-                    left= grid[i][j]+dp[i][j-1];
-                dp[i][j]=min(up,left);
-            }
-        }
-        return dp[r-1][c-1];
-    }
-};
-
-/*
-class Solution {
-    int minPathSum(vector<vector<int>>& grid,int r,int c, vector<vector<int>>&dp)
+    int f(vector<vector<int>>& grid,int currR,int currC,vector<vector<int>> &dp)
     {
-        if(r==0&&c==0)return dp[0][0]= grid[0][0];
-        if(r<0||c<0)return 1e9;
-        if(dp[r][c]!=-1)
-            return dp[r][c];
-        int up= grid[r][c]+minPathSum(grid,r-1,c,dp);
-        int left= grid[r][c]+minPathSum(grid,r,c-1,dp);
-        
-        return dp[r][c]=min(up,left);
+        if(currR<0||currC<0)
+            return 1e9;
+        if(currR==0&&currC==0)
+            return dp[currR][currC]=grid[0][0];
+        if(dp[currR][currC]!=-1)
+            return dp[currR][currC];
+        // cout<<currR<<" "<<currC<<endl;
+
+        int left= grid[currR][currC]+f(grid,currR,currC-1,dp);
+        int up=grid[currR][currC]+ f(grid,currR-1,currC,dp);
+        // cout<<left<<" "<<up<<endl;
+
+        return dp[currR][currC]=min(left,up);
     }
+    
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int r=grid.size();
         int c=grid[0].size();
-        vector<vector<int>>dp(r,vector<int>(c,-1));
-        return minPathSum(grid,r-1,c-1,dp);
+        vector<vector<int>> dp(r+1,vector<int>(c+1,-1));
+        
+        return f(grid,r-1,c-1,dp);
     }
 };
-*/

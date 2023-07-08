7244d7f9fae37be24ed933522b26c2a3e8c51d58
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-07-08 17:35:57 +0530

    Time: 40 ms (25.39%), Space: 24.9 MB (64.36%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,22 +2,28 @@ class Solution {
     
 public:
     void rotate(vector<int>& nums, int k) {
-        
+//         if(k==0||nums.size()<=1)
+//             return ;
+//       k=k%nums.size();
+//         if(k==0)
+//             return;
+//         vector<int>ans;
+
+//         for(int i=nums.size()-k;i<nums.size();i++)
+//            ans.push_back(nums[i]);
+//         for(int i=0;i<nums.size()-k;i++)
+//             ans.push_back(nums[i]);
+//         nums=ans;
+
         if(k==0||nums.size()<=1)
             return ;
       k=k%nums.size();
-        if(k==0)
-            return;
-      // cout<<nums.size()-k<<"nums.size()-k"<<endl;
-        vector<int>ans;
-       
-        for(int i=nums.size()-k;i<nums.size();i++)
-           ans.push_back(nums[i]);
 
-        
-        for(int i=0;i<nums.size()-k;i++)
-            ans.push_back(nums[i]);
-    
-        nums=ans;
+           if(k==0)
+             return ;
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+
     }
 };

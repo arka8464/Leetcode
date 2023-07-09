4ceff615007ed4f469f4f3a2fac23fac31b73959
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-07-10 00:12:57 +0530

    Time: 981 ms (5.06%), Space: 12.9 MB (35.87%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,57 +1,76 @@
 class Solution {
+    
 public:
-    string shortestCommonSupersequence(string s1, string s2) {
-       int l1=s1.size();
-       int l2=s2.size(); 
-        vector<vector<int>>dp(l1+1,vector<int>(l2+1,-1));
-        //we don't represent the base case involving 0 for negative indexes 
-        //instead we do them for zero 
-        for(int i=0;i<=l1;i++)dp[i][0]=0;
-        for(int i=0;i<=l2;i++)dp[0][i]=0;
+    string shortestCommonSupersequence(string str1, string str2) {
+        vector<vector<int>> dp(str1.size()+1,vector<int>(str2.size()+1,-1));
         
-        for(int i=1;i<=l1;i++)
+        for(int i=0;i<=str1.size();i++)
+            dp[i][0]=0;
+        
+         for(int i=0;i<=str2.size();i++)
+            dp[0][i]=0;
+        
+        
+        for(int i=1;i<=str1.size();i++)
         {
-            for(int j=1;j<=l2;j++)
+            for(int j=1;j<=str2.size();j++)
             {
-                 if(s1[i-1]==s2[j-1])
-            dp[i][j]= 1+ dp[i-1][j-1];
-        else 
-             dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                if(str1[i-1]==str2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                   dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
             }
-        } 
+        }
+          for(int i=0;i<=str1.size();i++)
+        {
+            for(int j=0;j<=str2.size();j++)
+            {
+                
+                cout<<dp[i][j]<<" ";
+                
+            }
+          cout<<endl;}
+        cout<<dp[str1.size()][str2.size()]<<endl;
         
         string ans="";
-        int i=l1,j=l2;
+        
+        int i=str1.size(),j=str2.size();
         
         while(i>0&&j>0)
         {
-           if(s1[i-1]==s2[j-1])
+           if(str1[i-1]==str2[j-1])
            {
-               ans+=s1[i-1];
+               ans+=str1[i-1];
                i--,j--; 
            }
             else if(dp[i-1][j]>dp[i][j-1])
             {
-                ans+=s1[i-1];
+                ans+=str1[i-1];
                 i--;
             }
             else
             {
-                 ans+=s2[j-1];
+                 ans+=str2[j-1];
                 j--;
             }
         }
         while(i>0)
         {
-            ans+=s1[i-1];
+            ans+=str1[i-1];
             i--;
         }
         while(j>0)
         {
-            ans+=s2[j-1];
+            ans+=str2[j-1];
             j--;
         }
         reverse(ans.begin(),ans.end());
         return ans;
+        // return "g";
+    
     }
 };

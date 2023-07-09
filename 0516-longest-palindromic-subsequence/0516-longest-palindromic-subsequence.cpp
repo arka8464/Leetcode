17dcb020d2f1e337e5ffe201913c75411474fb28
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-07-09 20:34:22 +0530

    Time: 249 ms (25.31%), Space: 73 MB (55.17%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -20,6 +20,32 @@ public:
         
         // cout<<s1<<" "<<s2;
         vector<vector<int>> dp(s1.size()+1,vector<int>(s1.size()+1,-1));
-        return f(s1,s2,s1.size()-1,s2.size()-1,dp);
-    }
+        // return f(s1,s2,s1.size()-1,s2.size()-1,dp);
+        for(int i=0;i<=s1.size();i++)
+            dp[i][0]=0;
+         for(int i=0;i<=s1.size();i++)
+            dp[0][i]=0;
+        
+        
+        for(int i=1;i<=s1.size();i++)
+         {
+              for(int j=1;j<=s1.size();j++)
+              {
+                 int match=-1e9, notMatch=-1e9;
+                 if(s1[i-1]==s2[j-1])
+                   dp[i][j]=1+dp[i-1][j-1];
+                 else
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                // dp[i][j]=max(match,notMatch); 
+              }
+         }
+        
+         // for(int i=0;i<=s1.size();i++)
+         // {
+         //      for(int j=0;j<=s1.size();j++)
+         //          cout<<dp[i][j]<<" ";
+         //     cout<<endl;
+         // }
+        return dp[s1.size()][s1.size()];
+        }
 };

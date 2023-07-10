4926155c6450083fbaf7a5253873343a5b9cbf96
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-07-10 20:20:24 +0530

    Time: 250 ms (34.63%), Space: 90.6 MB (42.16%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -12,7 +12,7 @@ class Solution {
             return dp[ind][canBuy];
         int profit=0;
         if(canBuy==true)
-           profit=max(f(prices,fee,ind+1,endInd,true,dp),f(prices,fee,ind+1,endInd,false,dp)-prices[ind]);
+           profit=max(f(prices,fee,ind+1,endInd,true,dp),f(prices,fee,ind+1,endInd,false,dp)- prices[ind]);
         else
            profit=max(f(prices,fee,ind+1,endInd,false,dp),f(prices,fee,ind+1,endInd,true,dp)+prices[ind]-fee);
         
@@ -21,7 +21,28 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
         int  n=prices.size();
-        vector<vector<int>> dp(prices.size()+1,vector<int>(3,-1));
-        return f(prices,fee,0,n-1,true,dp);
+        vector<vector<int>> dp(prices.size()+1,vector<int>(2,0));
+        if(n==0)
+            return 0;
+
+        
+        int canbuy=true;
+        for(int i=n-1;i>=0;i--)
+        {
+            int profit=0;
+            for(int canBuy=0;canBuy<=1;canBuy++)
+            {
+                if(canBuy==0)// We can buy the stock
+                    profit = max(0+dp[i+1][0], -prices[i] + dp[i+1][1]);
+            
+    
+            if(canBuy==1)// We can sell the stock
+                profit = max(0+dp[i+1][1], prices[i] -fee + dp[i+1][0]);
+            
+                
+            dp[i][canBuy]  = profit;
+            }
+        }
+        return dp[0][0];
     }
 };

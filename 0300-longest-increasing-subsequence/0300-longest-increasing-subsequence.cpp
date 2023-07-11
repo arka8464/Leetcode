5131c667003d643811102ff2e9614cde9904ce2a
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-07-11 11:24:52 +0530

    Time: 1525 ms (15.93%), Space: 292.5 MB (8.15%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,21 +1,26 @@
 class Solution {
+    int f(vector<int>& nums,int ind,int prev,vector<vector<int>> &dp)
+    {
+        // if(ind==nums.size()-1)
+        // {
+        //     if(nums[prev+1]<nums[ind])
+        //        return 1;
+        //     else
+        //         return 0;
+        // }
+        if(ind==nums.size())return 0;
+        
+        if(dp[ind][prev+1]!=-1)
+            return dp[ind][prev+1];
+        int pick=0;
+        if(prev==-1||nums[ind]>nums[prev])
+           pick=   1 +  f(nums,ind+1,ind,dp);
+        int notPick=f(nums,ind+1,prev,dp);
+        return  dp[ind][prev+1]=max(pick,notPick);
+    }
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,0));
-        for(int curr_ind=nums.size()-1;curr_ind>=0;curr_ind--)
-        {
-            for(int prev_ind=curr_ind-1;prev_ind>=-1;prev_ind--)
-            {
-                int take=-1e8;
-                int nottake=dp[curr_ind+1][prev_ind+1];
-              if(prev_ind==-1||nums[curr_ind]>nums[prev_ind])
-                 take=1+dp[curr_ind+1][curr_ind+1];
-              
-            dp[curr_ind][prev_ind+1]=max(take,nottake);
-            }
-        }
-        
-        return dp[0][-1+1];
-            
+        vector<vector<int>> dp(nums.size()+1,vector<int>(nums.size()+1,-1));
+        return f(nums,0,-1,dp);
     }
 };

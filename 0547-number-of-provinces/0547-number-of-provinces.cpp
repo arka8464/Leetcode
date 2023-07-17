8be7a14c0a28837c0cd2868359bbd5cdeb4b6ba5
arka8464 <69448901+arka8464@users.noreply.github.com>
2023-07-17 20:44:51 +0530

    Time: 24 ms (85.15%), Space: 15.4 MB (28.30%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,72 +1,23 @@
-class DisjointSet {
-public: 
-    vector<int> rank, parent, size; 
-
-    DisjointSet(int n) {
-        rank.resize(n+1, 0); 
-        parent.resize(n+1);
-        size.resize(n+1); 
-        for(int i = 0;i<=n;i++) {
-            parent[i] = i; 
-            size[i] = 1; 
-        }
-    }
-
-    int findUPar(int node) {
-        if(node == parent[node])
-            return node; 
-        return parent[node] = findUPar(parent[node]); 
-    }
-
-    void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u); 
-        int ulp_v = findUPar(v); 
-        if(ulp_u == ulp_v) return; 
-        if(rank[ulp_u] < rank[ulp_v]) {
-            parent[ulp_u] = ulp_v; 
-        }
-        else if(rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u; 
-        }
-        else {
-            parent[ulp_v] = ulp_u; 
-            rank[ulp_u]++; 
-        }
-    }
-
-    void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u); 
-        int ulp_v = findUPar(v); 
-        if(ulp_u == ulp_v) return; 
-        if(size[ulp_u] < size[ulp_v]) {
-            parent[ulp_u] = ulp_v; 
-            size[ulp_v] += size[ulp_u]; 
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v]; 
-        }
-    }
-};
 class Solution {
+    void dfs(int i, vector<vector<int>>& isConnected, vector<int>& visited) {
+        for (int j = 0; j < isConnected.size(); ++j) {
+            if (isConnected[i][j] == 1 && visited[j] == 0) {
+                visited[j] = 1;
+                dfs(j, isConnected, visited);
+            }
+        }
+    }
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int rw=isConnected.size();
-        int col=isConnected[0].size();
-        DisjointSet ds(rw);
-       for(int i=0;i<rw;i++)
-       {
-           for(int j=0;j<col;j++)
-          {
-               if(isConnected[i][j]==1)
-               {
-                                  ds.unionBySize(i,j);
-               }
-           }
-       }
-        int ans=0;
-        for(int i=0;i<rw;i++)
-            if(ds.parent[i]==i)ans++;
-    return ans;
+        int n = isConnected.size();
+        vector<int> visited(n, 0);
+        int provinces = 0;
+        for (int i = 0; i < n; ++i) {
+            if (visited[i] == 0) {
+                dfs(i, isConnected, visited);
+                ++provinces;
+            }
+        }
+        return provinces;
     }
 };
